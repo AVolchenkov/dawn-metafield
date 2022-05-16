957bf679c71da16223a5b933215ab039b84f95a5
MyrddinOV <predator.xnet@gmail.com>
2022-05-16 11:00:05 +0100

    BGCMMSHPFY-1787 - added ajax api call

--- assets/set-client-browser-cart.js
+++ assets/set-client-browser-cart.js
@@ -3,3 +3,21 @@ const userBrowserInfo = navigator.userAgent.toString();
 document.querySelector(".cart__checkout-button").addEventListener("click", function () {
   document.querySelector("#browser-name-cart").value = userBrowserInfo
 })
+
+window.onload = function () {
+  fetch(window.Shopify.routes.root + 'cart.js', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+};
